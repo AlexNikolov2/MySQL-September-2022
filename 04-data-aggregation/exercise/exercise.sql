dcb86abbef5983f7415ec9c35f5d6c85cb9c1ed0
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-28 22:39:39 +0300

    13 done

--- 04-data-aggregation/exercise/exercise.sql
+++ 04-data-aggregation/exercise/exercise.sql
@@ -50,3 +50,12 @@ WHERE `deposit_start_date` > '1985-01-01' GROUP BY `deposit_group` , `is_deposit
 --12
 SELECT  `department_id`, MIN(`salary`) AS `minimum_salary` FROM `employees`
 WHERE `department_id` IN (2 , 5, 7) GROUP BY `department_id` ORDER BY `department_id`;
+
+--13
+SELECT  `department_id`,
+    CASE
+        WHEN `department_id` = 1 THEN AVG(`salary`) + 5000
+        ELSE AVG(`salary`)
+    END AS `avg_salary`
+FROM `employees`
+WHERE `salary` > 30000 AND `manager_id` != 42 GROUP BY `department_id` ORDER BY `department_id`;
