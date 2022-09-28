b4d8046012a3ff0136551dcf0c01daf36fef0e76
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-28 22:01:46 +0300

    06 done

--- 04-data-aggregation/exercise/exercise.sql
+++ 04-data-aggregation/exercise/exercise.sql
@@ -7,3 +7,6 @@ SELECT `deposit_group`, MAX(`magic_wand_size`) AS `longest_magic_wand` FROM `wiz
 SELECT `deposit_group`  FROM `wizzard_deposits` GROUP BY `deposit_group` ORDER BY AVG(`magic_wand_size`) ASC LIMIT 1;
 
 SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum` FROM `wizzard_deposits` GROUP By `deposit_group` ORDER BY `total_sum` ASC;
+
+SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum` FROM `wizzard_deposits` 
+WHERE `magic_wand_creator` = 'Ollivander family' GROUP BY `deposit_group` ORDER BY `deposit_group` ASC;
