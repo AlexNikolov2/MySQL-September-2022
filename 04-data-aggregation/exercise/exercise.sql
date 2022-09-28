27d7b45e3b6408c95a78a30977111e63e6c3d3ba
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-28 22:12:33 +0300

    08 done

--- 04-data-aggregation/exercise/exercise.sql
+++ 04-data-aggregation/exercise/exercise.sql
@@ -20,3 +20,7 @@ WHERE `magic_wand_creator` = 'Ollivander family' GROUP BY `deposit_group` ORDER 
 --07
 SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum` FROM `wizzard_deposits` 
 WHERE `magic_wand_creator` = 'Ollivander family'  GROUP BY `deposit_group` HAVING `total_sum` < 150000 ORDER BY `total_sum` DESC;
+
+--08
+SELECT `deposit_group`, `magic_wand_creator`, MIN(`deposit_charge`) AS 'min_deposit_charge' FROM `wizzard_deposits`
+GROUP BY `deposit_group` , `magic_wand_creator` ORDER BY `magic_wand_creator` ASC , `deposit_group` ASC;
