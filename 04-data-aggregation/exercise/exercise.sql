50de0dcbb9158408c436fd85f8dd219d4b10252e
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-28 22:17:28 +0300

    10 done

--- 04-data-aggregation/exercise/exercise.sql
+++ 04-data-aggregation/exercise/exercise.sql
@@ -38,3 +38,7 @@ SELECT
     END AS `age_group`,
     COUNT(*) AS `wizard_count`
 FROM `wizzard_deposits` GROUP BY `age_group` ORDER BY `wizard_count` ASC;
+
+--10
+SELECT  LEFT(`first_name`, 1) AS `first_letter` FROM `wizzard_deposits`
+WHERE `deposit_group` = 'Troll Chest' GROUP BY `first_letter` ORDER BY `first_letter`;
