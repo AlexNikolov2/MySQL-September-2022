b2fed75ab7b278c1736ad6c408509fae03790237
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-28 21:53:19 +0300

    04 done

--- 04-data-aggregation/exercise/exercise.sql
+++ 04-data-aggregation/exercise/exercise.sql
@@ -3,3 +3,5 @@ SELECT COUNT(`id`) AS `count` FROM `wizzard_deposits`;
 SELECT MAX(`magic_wand_size`) AS `longest_magic_wand` FROM `wizzard_deposits`;
 
 SELECT `deposit_group`, MAX(`magic_wand_size`) AS `longest_magic_wand` FROM `wizzard_deposits` GROUP BY `deposit_group` ORDER BY `longest_magic_wand`, `deposit_group`;
+
+SELECT `deposit_group`  FROM `wizzard_deposits` GROUP BY `deposit_group` ORDER BY AVG(`magic_wand_size`) ASC LIMIT 1;
