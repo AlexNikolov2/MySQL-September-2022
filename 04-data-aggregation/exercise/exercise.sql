bd2628884bf91bd94d8603c0bda497b1a676edf0
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-28 22:57:22 +0300

    18 done

--- 04-data-aggregation/exercise/exercise.sql
+++ 04-data-aggregation/exercise/exercise.sql
@@ -82,3 +82,6 @@ WHERE `salary` > (
         WHERE `e`.`department_id` = `employees`.`department_id`
         )
 ORDER BY `department_id` ASC , `employee_id` ASC LIMIT 10;
+
+--18
+SELECT `department_id`, SUM(`salary`) AS `total_salary` FROM `employees`  GROUP BY `department_id` ORDER BY `department_id`;
