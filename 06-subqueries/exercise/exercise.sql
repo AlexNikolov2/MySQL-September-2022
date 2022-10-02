bf1f92efaee4e53e0efbf0a826f9fe53146afe1c
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-02 03:42:50 +0300

    17 done

--- 06-subqueries/exercise/exercise.sql
+++ 06-subqueries/exercise/exercise.sql
@@ -93,3 +93,10 @@ SELECT  COUNT(*) AS `country_count` FROM `countries`
 WHERE`country_code` NOT IN (
 	SELECT `country_code` FROM `mountains_countries`
     );
+
+--17
+
+SELECT  `country_name`, MAX(p.`elevation`) AS `highest_peak_elevation`, MAX(r.`length`) AS `longest_river_length`
+FROM `countries` AS c JOIN `mountains_countries` AS mc ON c.`country_code` = mc.`country_code` 
+JOIN `peaks` AS p ON mc.`mountain_id` = p.`mountain_id` JOIN `countries_rivers` AS cr ON c.`country_code` = cr.`country_code` JOIN `rivers` AS r ON cr.`river_id` = r.`id`
+GROUP BY c.`country_name` ORDER BY `highest_peak_elevation` DESC , `longest_river_length` DESC , c.`country_name` ASC LIMIT 5;
