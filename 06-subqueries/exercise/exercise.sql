d5cbeebae6c64c900ee66e58d2360b300cf01689
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-02 03:15:31 +0300

    08 done

--- 06-subqueries/exercise/exercise.sql
+++ 06-subqueries/exercise/exercise.sql
@@ -42,3 +42,12 @@ WHERE DATE(e.`hire_date`) > '1999-01-01' AND d.`name` IN ('Sales' , 'Finance') O
 SELECT  e.`employee_id`, e.`first_name`, p.`name` AS `project_name`
 FROM `employees` AS e JOIN `employees_projects` AS ep ON e.`employee_id` = ep.`employee_id` JOIN `projects` AS p ON ep.`project_id` = p.`project_id`
 WHERE DATE(p.`start_date`) > '2002-08-13' AND p.`end_date` IS NULL ORDER BY e.`first_name` ASC , p.`name` ASC LIMIT 5;
+
+--08
+
+SELECT e.`employee_id`, e.`first_name`,	IF(
+		YEAR(p.`start_date`) > 2004, NULL,
+        p.`name`
+        ) AS `project_name`
+FROM `employees` AS e JOIN `employees_projects` AS ep ON e.`employee_id` = ep.`employee_id` JOIN `projects` AS p ON ep.`project_id` = p.`project_id`
+WHERE e.`employee_id` = 24 ORDER BY p.`name` ASC;
