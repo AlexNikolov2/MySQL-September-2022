4e162855a8a1c7350c3135522cbc45d076d3f25f
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-02 03:09:35 +0300

    06 done

--- 06-subqueries/exercise/exercise.sql
+++ 06-subqueries/exercise/exercise.sql
@@ -30,3 +30,9 @@ WHERE e.`employee_id` NOT IN (
 	SELECT `employee_id` FROM `employees_projects`
     )
 ORDER BY e.`employee_id` DESC LIMIT 3;
+
+--06
+
+SELECT  e.`first_name`, e.`last_name`, e.`hire_date`, d.`name` AS `dept_name`
+FROM `employees` AS e JOIN `departments` AS d ON e.`department_id` = d.`department_id`
+WHERE DATE(e.`hire_date`) > '1999-01-01' AND d.`name` IN ('Sales' , 'Finance') ORDER BY e.`hire_date` ASC;
