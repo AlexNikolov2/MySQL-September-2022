5f879d856045fbf00d7ec721892019389b9aa242
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-02 03:03:34 +0300

    05 done

--- 06-subqueries/exercise/exercise.sql
+++ 06-subqueries/exercise/exercise.sql
@@ -21,3 +21,12 @@ WHERE d.`name` = 'Sales' ORDER BY e.`employee_id` DESC;
 SELECT  e.`employee_id`, e.`first_name`, e.`salary`, d.`name` AS `department_name`
 FROM `employees` AS e JOIN `departments` AS d ON e.`department_id` = d.`department_id`
 WHERE e.`salary` > 15000 ORDER BY d.`department_id` DESC LIMIT 5;
+
+--05
+
+SELECT  e.`employee_id`, e.`first_name`
+FROM `employees` AS e
+WHERE e.`employee_id` NOT IN (
+	SELECT `employee_id` FROM `employees_projects`
+    )
+ORDER BY e.`employee_id` DESC LIMIT 3;
