ab434bbaa0dc91c00086b4785ced22a25d33ab52
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-07 18:24:48 +0300

    01 done

--- 07-programmility-transactions/exercise/exercise.sql
+++ 07-programmility-transactions/exercise/exercise.sql
@@ -1,2 +1,8 @@
 --01
 
+create procedure usp_get_employees_salary_above_35000()
+BEGIN
+	select `first_name`, `last_name` from `employees` where `salary` > 35000 order by `first_name` asc, `last_name` asc, `employee_id` asc;
+end
+
+--02
