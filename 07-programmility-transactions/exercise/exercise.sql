ee05f84af672746b2f095744912eac63ead2eecf
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-07 19:37:51 +0300

    06 done

--- 07-programmility-transactions/exercise/exercise.sql
+++ 07-programmility-transactions/exercise/exercise.sql
@@ -39,4 +39,11 @@ CREATE FUNCTION ufn_get_salary_level(salary_input DECIMAL(10,4)) RETURNS VARCHAR
 		END
     );
 
-    --06
+--06
+
+CREATE PROCEDURE usp_get_employees_by_salary_level(salary_level VARCHAR(7))
+BEGIN
+	SELECT `first_name`, `last_name` FROM `employees` WHERE `salary` < 30000 AND salary_level = 'low'
+    OR `salary` >= 30000 AND `salary` <= 50000 AND salary_level = 'average' OR `salary` > 50000 AND salary_level = 'high'
+    ORDER BY `first_name` DESC, `last_name` DESC;
+END
