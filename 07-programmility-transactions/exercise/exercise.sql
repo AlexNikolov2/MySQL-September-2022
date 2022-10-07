2ad0392aa53463649caa931cb2ae0c2c1c050e7e
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-07 18:26:37 +0300

    02 done

--- 07-programmility-transactions/exercise/exercise.sql
+++ 07-programmility-transactions/exercise/exercise.sql
@@ -6,3 +6,10 @@ BEGIN
 end
 
 --02
+
+create procedure usp_get_employees_salary_above(target_salary DECIMAL(10,4))
+BEGIN
+	select `first_name`, `last_name` from `employees` where `salary` >= target_salary order by `first_name` asc, `last_name` asc, `employee_id` asc;
+end
+
+--03
