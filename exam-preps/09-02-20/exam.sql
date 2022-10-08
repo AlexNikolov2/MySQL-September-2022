f5cac6d9a6a3d28d01b3f363faedd3c690e65267
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-09 00:14:50 +0300

    01 done

---
+++ exam-preps/09-02-20/exam.sql
@@ -0,0 +1,7 @@
+--01
+
+
+--02
+
+insert into `coaches` (`first_name`, `last_name`, `salary`, `coach_level`) SELECT `first_name`, `last_name`, `salary` * 2, char_length(`first_name`) FROM `players` WHERE `age` >= 45
+
