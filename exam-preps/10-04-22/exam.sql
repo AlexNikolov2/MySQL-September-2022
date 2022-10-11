362cb1e74e2d5871aa8daf0c51974e531edfde4d
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-12 01:01:49 +0300

    03

--- exam-preps/10-04-22/exam.sql
+++ exam-preps/10-04-22/exam.sql
@@ -75,3 +75,9 @@ CREATE TABLE `genres_movies`
 INSERT INTO `actors`(`first_name`, `last_name`, `birthdate`, `height`, `awards`, `country_id`)
 SELECT (REVERSE(a.`first_name`)),(REVERSE(a.`last_name`)),(DATE (a.`birthdate` - 2)),(a.`height` + 10),(a.`country_id`),(3) FROM `actors` as a
 WHERE a.`id` <= 10;
+
+--03
+
+UPDATE `movies_additional_info` m
+SET m.`runtime` = m.`runtime` - 10
+WHERE m.`id` BETWEEN 15 AND 25;
