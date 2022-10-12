8f493ec90fe9e903ca02cacb026dba121671a2b7
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-12 22:29:50 +0300

    11

--- exam-preps/10-04-22/exam.sql
+++ exam-preps/10-04-22/exam.sql
@@ -156,4 +156,13 @@ BEGIN
     RETURN movies_count;
     END
 
---11
+--
+
+CREATE PROCEDURE `udp_award_movie`(`movie_title` VARCHAR(50))
+	BEGIN
+		UPDATE actors as a
+			JOIN `movies_actors` as ma on a.id = ma.actor_id
+            JOIN movies as m on m.id = ma.movie_id
+		SET a.awards = a.awards + 1
+        WHERE m.title = movie_title;
+END
