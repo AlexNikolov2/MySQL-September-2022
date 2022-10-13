7aea37998a0236b16fa6a0e1d535aeff8966b1e2
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-14 01:23:23 +0300

    09

--- exam-preps/09-06-19/exam.sql
+++ exam-preps/09-06-19/exam.sql
@@ -105,3 +105,10 @@ ORDER BY `count_of_clients` DESC , `employee_id` ASC LIMIT 5;
 
 --09
 
+SELECT  b.`name`, COUNT(ca.`id`) AS `count_of_cards` FROM `branches` AS b
+	LEFT JOIN `employees` AS e ON b.`id` = e.`branch_id`
+	LEFT JOIN `employees_clients` AS ec ON e.`id` = ec.`employee_id`
+	LEFT JOIN `clients` AS c ON ec.`client_id` = c.`id`
+	LEFT JOIN `bank_accounts` AS ba ON c.`id` = ba.`client_id`
+	LEFT JOIN `cards` AS ca ON ba.`id` = ca.`bank_account_id`
+GROUP BY b.`name` ORDER BY `count_of_cards` DESC , b.`name` ASC;
