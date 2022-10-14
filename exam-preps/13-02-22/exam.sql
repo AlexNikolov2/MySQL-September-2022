3c2e65003f407205835560882506ff94bfd42485
Aleks Nikolov <tcr.official.world@gmail.com>
2022-10-14 21:20:32 +0300

    09

--- exam-preps/13-02-22/exam.sql
+++ exam-preps/13-02-22/exam.sql
@@ -103,3 +103,9 @@ order by `rating` desc;
 select concat(`first_name`, ' ', `last_name`) as `full_name`, `address`, `order_datetime` from `customers` join `orders` on `customers`.`id` = `orders`.`customer_id` where year(`order_datetime`) <= 2018
 order by `full_name` desc;
 
+--09
+
+select count(`p`.`id`) as `items_count`, `c`.`name`, sum(`p`.`quantity_in_stock`) as `total_quantity` from `products` as `p`
+join `categories` as `c` on `p`.`category_id` = `c`.`id`
+group by `category_id`
+order by `items_count` desc, `total_quantity` limit 5;
